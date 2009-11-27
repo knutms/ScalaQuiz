2701f3b72d415330f0b120e0f2f054845d11a555
Kjell Martin Rud <kjellmartinrud@ow-317-mac.intra.itera.no>
2009-11-27 19:57:44 +0100

    Added project structure.

---
+++ quiz-common/src/main/scala/no/objectware/game/App.scala
@@ -0,0 +1,9 @@
+package no.objectware.game
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ quiz-frontend/src/main/scala/no/objectware/game/snippet/HelloWorld.scala
@@ -0,0 +1,6 @@
+package no.objectware.game.snippet
+
+class HelloWorld {
+  def howdy = <span>Welcome to quiz-frontend at {new _root_.java.util.Date}</span>
+}
+

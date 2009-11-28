48a9d59dacc9308b363f8f9ca1f3d63ec9bd5f88
Knut Magne <knutms@knutms-laptop.(none)>
2009-11-28 15:49:53 +0100

    added test

--- quiz-core/src/main/scala/no/objectware/quiz/MasterActor.scala
+++ quiz-core/src/main/scala/no/objectware/quiz/MasterActor.scala
@@ -5,22 +5,49 @@ import messages._
 import no.objectware.quiz.{Result => ResultObject}
 import scala.actors.Actor._
 import scala.actors.remote.RemoteActor._
+import scala.actors._
 
-class MasterActor extends Actor {
+class MasterActor(val quizActor : QuizRepositoryActor) extends Actor {
 
   start()
-
+  
+  var players : List[(Player with Socket)] = Nil
+  private var currentQuestion = 0
+  private var unansweredPlayers : List[Player] = Nil
+  
+  
   def act = {
     alive(9000)
     register('master, self)
     loop {
       receive {
-        case Join(player) => reply(Welcome(player :: Nil))
+        case Join(player) => {
+          println("Joining " + player)
+	        players = (new Player(player.name) with Socket {socket = sender}) :: players
+	        if (currentQuestion == 0) {
+	          generateQuestions()
+            }
+	        reply(Welcome(players))
+         }
         case Leave => reply(Goodbye("Good bye! See you next time."))
+        case q: Question => {
+	        println("Distributing question")
+	        players.foreach(_.socket ! q)
+          }
         case Answer(id, choice, timeUsed) => reply(Result(id, id, MasterActor.createPlayerWithResult("You", id, timeUsed, 1) :: Nil))
       }
     }
   }
+  
+  def generateQuestions() = {
+	unansweredPlayers = players
+	quizActor ! 'generateQuestion
+  }
+}
+
+trait Socket {
+  var socket : OutputChannel[Any] = null
+  override def toString = super.toString() + " with socket: " + socket
 }
 
 object MasterActor {
@@ -33,11 +60,11 @@ object MasterActor {
       def rank = rank
     }
   }
-
 }
 
 object MasterBootStrapper {
   def main(args: Array[String]) {
-    new MasterActor
+    val quizRepositoryActor = new QuizRepositoryActor();
+    new MasterActor(quizRepositoryActor)
   }
 }
--- quiz-core/src/test/scala/no/objectware/quiz/QuizGameTest.scala
+++ quiz-core/src/test/scala/no/objectware/quiz/QuizGameTest.scala
@@ -5,6 +5,12 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Assert._
 
+import no.objectware.quiz.messages._
+import scala.actors._
+import scala.actors.Actor._
+
+import java.util.concurrent.CyclicBarrier
+
 @RunWith(classOf[JUnit4])
 class QuizGameTest {
 
@@ -15,4 +21,29 @@ class QuizGameTest {
     assertEquals(0, 0);
   }
 
+  @Test
+  def startGameAndJoin() {
+      val quizActor = new QuizRepositoryActor
+      val main = new MasterActor(quizActor)
+      
+      val twoThreadBarrier = new CyclicBarrier(2)
+      
+	  def playerMock = actor {
+	    loop {
+	      receive {
+	        case 'start => main ! Join(Player("Per"))
+	        case x: Welcome => println("I'm welcomed!")
+	        case q: Question => twoThreadBarrier.await() ; println("I got a question: " + q + " hmm.. not sure")
+	        case x => println("Got unknwn: " + x)
+	      }
+	    }
+	  }
+  
+      playerMock ! 'start
+    
+      twoThreadBarrier.await()
+      
+      assertEquals(1, main.players.size)
+  }
+  
 }
